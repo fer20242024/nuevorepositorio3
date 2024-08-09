b830cd3230d80b2c4ac252791e8d85e51618ef0c
fer20242024 <anzoasistemas.2019@gmail.com>
2024-08-09 17:27:56 -0400

    Envio de paquete para revision

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
+      const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+    const handleAddTodo = () => {
+    if (headingInput.trim() !== '') {
+      setTodos([...todos, { heading: headingInput, lists: [] }]);
+      setHeadingInput('');
+    }
+  };
   
 
   return (
@@ -13,12 +22,23 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
+            value={headingInput}
+    onChange={(e) => {setHeadingInput(e.target.value);}} 
+    // Add onChange event handler to update headingInput state
             
           />
           <button className="add-list-button">Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
+        {todos.map((todo, index) => (
+    <div key={index} className="todo-card">
+      <div className="heading_todo">
+        <h3>{todo.heading}</h3> {/* Display the heading here */}
+        <button className="delete-button-heading">Delete Heading </button>
+      </div>
+    </div>
+  ))}
         
       </div>
     </>
